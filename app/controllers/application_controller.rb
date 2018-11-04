20ec86c0daf405c5b61b31e4df225151e69f02c3
Ikegami Jumpei <j.ikegami010125@gmail.com>
2018-11-05 02:34:56 +0900

    Rails06

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   add_flash_types :success, :info, :warning, :danger
+
+  class HelperImpl
+    include ::Singleton
+    include ::ApplicationHelper
+  end
+
+  protected
+
+  def helper
+    return HelperImpl.instance
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
